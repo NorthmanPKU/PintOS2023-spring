c50d62d1808fd299c1a1090af74fd8a9a372fe85
NorthmanPKU <jjn20020813@163.com>
2023-06-16 21:16:48 +0800

    class over

--- src/userprog/syscall.h
+++ src/userprog/syscall.h
@@ -6,12 +6,12 @@ extern struct lock sup_page_lock;
 extern struct lock lock_for_scan;
 typedef uint32_t mapid_t;
 struct mmap_file {
-    mapid_t mapid;
-    int fd;
-    struct file *file;
-    void *addr;
-    int length;
-    struct list_elem elem;
+    mapid_t mapid; //id of the mapped file
+    int fd; //file descriptor
+    struct file *file; //file pointer
+    void *addr; //address
+    int length; //length of the file
+    struct list_elem elem; //list element
 };
 
 void syscall_init (void);
